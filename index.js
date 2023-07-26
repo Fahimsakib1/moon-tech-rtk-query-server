fac77159cfc2aac6f272b29ab227cb1239a1a645
Fahim <60335100+Fahimsakib1@users.noreply.github.com>
2023-07-27 01:06:25 +0600

    setup server

---
+++ index.js
@@ -0,0 +1,56 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//set up middle wares
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+//require dotenv
+require('dotenv').config();
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.c2bp6su.mongodb.net/?retryWrites=true&w=majority`;
+console.log("Mongo set up Moon Tech: ", uri);
+
+
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+async function run() {
+    try {
+        const productsCollection = client.db('Moon_Tech_Redux_Thunk').collection('products');
+        
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const result = await productsCollection.find(query).toArray()
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(error => console.log(error))
+
+app.get("/", (req, res) => {
+    res.send("Moon Tech Redux Thunk Server!");
+});
+
+app.listen(port, () => {
+    console.log(`Moon Tech Redux Thunk Running on port ${port}`);
+});
